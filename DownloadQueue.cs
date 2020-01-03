d5b6cf790f1d6f3970b21c51ee51a3ff41f598b2
Lomeli <anthonylomeli11@gmail.com>
2020-01-03 01:11:26 -0800

    Changed buildWindows.bat to not use specific paths.

---
+++ DownloadQueue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FuseeGUI {
+    public class DownloadQueu {
+        private IList<DownloadFile> downloads;
+
+        public async Task runDownloads() {
+            
+        }
+    }
+}
--- buildWindows.bat
+++ buildWindows.bat
@@ -1,6 +1,6 @@
 dotnet publish -c Release -r win-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
 dotnet publish -c Release -r linux-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
 dotnet publish -c Release -r osx-x64 /p:PublishSingleFile=true /p:PublishTrimmed=true
-copy nintendo-switch.png "C:\Users\Lomeli\Documents\Repos\FuseeGUI\bin\Release\netcoreapp3.0\linux-x64\publish\nintendo-switch.png"
-copy nintendo-switch.png "C:\Users\Lomeli\Documents\Repos\FuseeGUI\bin\Release\netcoreapp3.0\osx-x64\publish\nintendo-switch.png"
-copy nintendo-switch.png "C:\Users\Lomeli\Documents\Repos\FuseeGUI\bin\Release\netcoreapp3.0\win-x64\publish\nintendo-switch.png"
+copy nintendo-switch.png bin\Release\netcoreapp3.0\linux-x64\publish\
+copy nintendo-switch.png bin\Release\netcoreapp3.0\osx-x64\publish\
+copy nintendo-switch.png bin\Release\netcoreapp3.0\win-x64\publish\
