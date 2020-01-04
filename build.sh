43d1a3d353fbd15d12f9f4045adae8a84015d561
Lomeli <anthonylomeli11@gmail.com>
2020-01-03 16:10:19 -0800

    Added a better build script.
    ...in my opinion...

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+PLATFORM="null"
+
+if [[ $1 -eq 0 ]]; then
+    if [[ $1 == "linux" || $1 == "osx" || $1 == "win" ]]; then
+        PLATFORM=$1
+    fi
+else
+    case "$OSTYPE" in
+        "linux-gnu"*)
+            PLATFORM="linux"
+            ;;
+        "darwin"*)
+            PLATFORM="osx"
+            ;;
+        "msys"* | "win32"*)
+            PLATFORM="win"
+            ;;
+        *)
+            echo "How'd we get here?"
+            exit
+            ;;
+    esac
+fi
+
+if [[ PLATFORM != "null" ]]; then
+    echo "Target Platform = $PLATFORM"
+    dotnet publish -c Release -r $PLATFORM-x64 //p:PublishSingleFile=true //p:PublishTrimmed=true
+    cp nintendo-switch.png "bin\\\Release\netcoreapp3.0\\$PLATFORM-x64\publish\\"
+fi
